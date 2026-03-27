804d9c0560b41e0c092aacbae30e9d0e5879c6ee
Aura Tao <auratao.model@gmail.com>
2026-03-27 15:33:53 +0100

    chore: changed the og titl

--- Hora/src/App.jsx
+++ Hora/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       <BrowserRouter>
         <a
           href="#main-content"
-          className="sr-only focus:not-sr-only focus:fixed focus:top-4 focus:left-4 focus:z-[9999] focus:px-4 focus:py-2 focus:bg-secondary focus:text-white focus:rounded focus:text-sm focus:font-semibold"
+          className="sr-only focus:not-sr-only focus:fixed focus:top-4 focus:left-4 focus:z-9999 focus:px-4 focus:py-2 focus:bg-secondary focus:text-white focus:rounded focus:text-sm focus:font-semibold"
         >
           Skip to main content
         </a>
